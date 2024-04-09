5d5ae29885c708c44aa6dec0127cbac0a57ee067
Fausto Fernandez <fausto44190084@gmail.com>
2024-04-08 21:11:33 -0300

    enviamos un props al componente informacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Informacion from './components/Informacion';
+
 
 function App() {
 
   return (
   <main className='container my-5'>
         <h1>Primer proyecto con react</h1>
+        <Informacion comision='c89i'></Informacion>
+        
         
   </main>
   )
