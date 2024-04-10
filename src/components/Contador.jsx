2ae4289211bacb833bea6c4f546704637a75c13e
Fausto Fernandez <fausto44190084@gmail.com>
2024-04-10 20:38:26 -0300

    agregamos la funcion sumar

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+
 const Contador = () => {
-     
+    //aqui agrego la logica del componente
+    // let numero = 10;
+    //aqui creo el state
+
+    const [numero, setNumero] = useState(1);
+
+
+    const sumar = ()=>{
+
+        setNumero(numero+1);
+          console.log(numero);
+    }
+
         return (
+            //aqui puedo agregar un poquito de logica
             <section className="text-center">
+               { /*aqui va la logca del maquetado*/}
                 <h2>Contador</h2>
-                <p>1</p>
-                <button className="btn btn-primary me-2">+1</button>
+                <p>{numero}</p>
+                <button className="btn btn-primary me-2" onClick={sumar}>+1</button>
                 <button className="btn btn-danger">-1</button>
             </section>
         );
