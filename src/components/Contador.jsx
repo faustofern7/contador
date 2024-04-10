1b546a26ffec55028e9fcc54bca6710133b8eedd
Fausto Fernandez <fausto44190084@gmail.com>
2024-04-10 20:44:22 -0300

    realice la funcion restar

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,30 +1,33 @@
 import { useState } from "react";
 
 const Contador = () => {
-    //aqui agrego la logica del componente
-    // let numero = 10;
-    //aqui creo el state
+  //aqui agrego la logica del componente
+  //aqui creo el state
+  const [numero, setNumero] = useState(1);
 
-    const [numero, setNumero] = useState(1);
+  return (
+    //aqui puedo agregar un poquito de logica
+    <section className="text-center">
+      {/*aqui va la logca del maquetado*/}
 
+      <h2>Contador</h2>
+      <p>{numero}</p>
+      
+      <button
+        className="btn btn-primary me-2"
+        onClick={() => setNumero(numero + 1)} //hago asi pq la funcion tiene una unica linea
+      >
+        +1
+      </button>
 
-    const sumar = ()=>{
+      <button
+        className="btn btn-danger me-2"
+        onClick={() => setNumero(numero - 1)} //hago asi pq la funcion tiene una unica linea
+      >
+        -1
+      </button>
 
-        setNumero(numero+1);
-          console.log(numero);
-    }
-
-        return (
-            //aqui puedo agregar un poquito de logica
-            <section className="text-center">
-               { /*aqui va la logca del maquetado*/}
-                <h2>Contador</h2>
-                <p>{numero}</p>
-                <button className="btn btn-primary me-2" onClick={sumar}>+1</button>
-                <button className="btn btn-danger">-1</button>
-            </section>
-        );
-    
-}
-
+    </section>
+  );
+};
 export default Contador;
